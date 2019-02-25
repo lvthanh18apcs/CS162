d25e6d027151e51b4a4673e7898f529706742332
Khoa Nguyen <khoa3101@gmail.com>
2019-02-25 14:21:46 +0700

    Initial Commit

---
+++ Student Management/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Student Management
+//
+//  Created by Khoa Nguyen on 2/25/19.
+//  Copyright © 2019 Khoa Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
