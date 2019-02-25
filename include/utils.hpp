b7208becb25c81b0775775499f9d1f7cef2e8a36
The San Cao <caothesan@gmail.com>
2019-02-25 15:54:41 +0700

    Add utility

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,27 +1,21 @@
 #ifndef UTILS_H_DEFINED
 #define UTILS_H_DEFINED
 
-#include <string>
 #include <vector>
+#include <string>
+#include <fstream>
 #include <cstdlib>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 namespace Utils {
     namespace Tool {
-        void createFolder(string path) {
-            system(("mkdir -p" + path).c_str());
-        }
-
-        void openInputFile(ifstream &fin, string path) {
-            fin.open(path);
-            if (!fin.is_open()) 
-        }
+        void createFolder(string path);
+        void openInputFile(ifstream &fin, string path);
         
         void openOutputFile(ifstream &fout, string path);
-        
-        void 
         
         int findNext(string s, int i, char value);
         
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,21 @@
 #include <utils.hpp>
 
+void Utils::Tool::createFolder(string path) {
+    system(("mkdir -p" + path).c_str());
+}
+
+void Utils::Tool::openInputFile(ifstream &fin, string path) {
+    fin.open(path);
+    if (!fin.is_open()) {
+        cout << "Error: " << path << " doesn't exist!\n";
+    }
+}
+
+void Utils::Tool::openOutputFile(ifstream &fout, string path) {
+    fout.open(path);
+}
+
+
 int Utils::Tool::findNext(string s, int i, int value) {
     return find(s.begin() + i, s.end(), value) - s.begin();
 }
